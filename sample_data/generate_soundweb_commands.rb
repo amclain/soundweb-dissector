2219ca070b9a2e699ad51fd7919f758914c90c8f
Alex McLain <alex@alexmclain.com>
2014-11-03 16:03:31 -0800

    Packet generator script handles checksum and HiQnet address.

--- sample_data/generate_soundweb_commands.rb
+++ sample_data/generate_soundweb_commands.rb
@@ -12,17 +12,35 @@ DI_BUMPSVPERCENT          = 0x90
 
 gem 'ionian', '~>0.6.10'
 require 'ionian'
+require 'ostruct'
 
 SOUNDWEB_IP   = '10.0.5.30'
 SOUNDWEB_PORT = 1023
 
 # Special bytes.
 # See "London DI Kit.pdf" page 4
-STX = 0x02
-ETX = 0x03
-ACK = 0x06
-NAK = 0x15
-ESC = 0x1B
+SPECIAL_BYTES = [
+  STX = 0x02,
+  ETX = 0x03,
+  ACK = 0x06,
+  NAK = 0x15,
+  ESC = 0x1B,
+]
+
+hiqnet_address = OpenStruct.new \
+  node:           0x1002,
+  virtual_device: 0x03,
+  object:         0x000147,
+  sv:             0x0000
+
+hiqnet_address.define_singleton_method(:to_a) {
+  [
+    node >> 8 & 0xFF, node & 0xFF,
+    virtual_device,
+    object >> 16 & 0xFF, object >> 8 & 0xFF, object & 0xFF,
+    sv >> 8 & 0xFF, sv & 0xFF
+  ]
+}
 
 # Message Format
 #   <message> = <STX> <body> <checksum byte> <ETX>
@@ -41,16 +59,21 @@ ESC = 0x1B
 # 
 # @see "London DI Kit.pdf" page 5
 def make_message body
+  checksum = body.reduce(0) { |a, byte| a ^ byte }
+  
   [
     STX,
-    *body,
-    body.reduce(0) { |a, byte| a ^ byte }, # Checksum
+    *(body + [checksum]).map { |byte|
+      # Escape special bytes in a message.
+      # @see "London DI Kit.pdf" page 4
+      SPECIAL_BYTES.include?(byte) ? [ESC, byte + 0x80] : byte
+    }.flatten,
     ETX
   ]
 end
 
 packets = [
-  make_message([DI_SETSVPERCENT, 0x10, 0x1B, 0x82, 0x1B, 0x83, 0x00, 0x01, 0x47, 0x00, 0x00, 0x00, 0x64, 0x00, 0x00])
+  make_message([DI_SETSVPERCENT, *hiqnet_address.to_a, 0x00, 0x00, 0x00, 0x64])
 ]
 
 Ionian::Socket.new host: "#{SOUNDWEB_IP}:#{SOUNDWEB_PORT}" do |socket|
@@ -59,3 +82,5 @@ Ionian::Socket.new host: "#{SOUNDWEB_IP}:#{SOUNDWEB_PORT}" do |socket|
     socket.flush
   end
 end
+
+puts "Done."
